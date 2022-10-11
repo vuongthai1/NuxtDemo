f687b43be604aec955afc15e21b6cc99456b4553
AES-PF1BLQPA\AES-LeKhoa <dangkhoa.le@asiaesolutions.com>
2022-10-11 11:25:19 +0700

    nuxt demo

---
+++ components/List.vue
@@ -0,0 +1,214 @@
+<template>
+  <v-card
+    style="
+      background: #3a1c71;
+      background: -webkit-linear-gradient(to right, #ffaf7b, #d76d77, #3a1c71);
+      background: linear-gradient(to right, #ffaf7b, #d76d77, #3a1c71);
+    "
+  >
+    <v-spacer></v-spacer>
+    <v-row class="v-row">
+      <v-col class="max-w-sm rounded overflow-hidden shadow-lg mb-2">
+        <v-img
+          lazy-src="https://picsum.photos/id/11/10/6"
+          max-height="150"
+          max-width="250"
+          src="https://picsum.photos/id/11/500/300"
+        />
+        <div class="px-6 py-4 pb-2">
+          <div class="font-bold text-xl mb-2 text-no-wrap">Green Glass</div>
+          <p class="text-gray-700 text-base text-no-wrap">
+            Death is like the wind
+          </p>
+        </div>
+      </v-col>
+      <v-col class="max-w-sm rounded overflow-hidden shadow-lg mb-2">
+        <v-img
+          class="w-full"
+          src="https://tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+          lazy-src="https://picsum.photos/id/11/10/6"
+          max-height="150"
+          max-width="250"
+        />
+        <div class="px-6 py-4 pb-2">
+          <div class="font-bold text-xl mb-2 text-no-wrap">
+            The Coldest Sunset
+          </div>
+          <p class="text-gray-700 text-base text-no-wrap">
+            Death is like the wind
+          </p>
+        </div>
+      </v-col>
+      <v-col class="max-w-sm rounded overflow-hidden shadow-lg mb-2">
+        <v-img
+          class="w-full"
+          src="https://tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+          lazy-src="https://picsum.photos/id/11/10/6"
+          max-height="150"
+          max-width="250"
+        />
+        <div class="px-6 py-4 pb-2">
+          <div class="font-bold text-xl mb-2 text-no-wrap">
+            The Coldest Sunset
+          </div>
+          <p class="text-gray-700 text-base text-no-wrap">
+            Death is like the wind
+          </p>
+        </div>
+      </v-col>
+      <v-col class="max-w-sm rounded overflow-hidden shadow-lg mb-2">
+        <v-img
+          class="w-full"
+          src="https://tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+          lazy-src="https://picsum.photos/id/11/10/6"
+          max-height="150"
+          max-width="250"
+        />
+        <div class="px-6 py-4 pb-2">
+          <div class="font-bold text-xl mb-2 text-no-wrap">
+            The Coldest Sunset
+          </div>
+          <p class="text-gray-700 text-base text-no-wrap">
+            Death is like the wind
+          </p>
+        </div>
+      </v-col>
+      <v-col class="max-w-sm rounded overflow-hidden shadow-lg mb-2">
+        <v-img
+          class="w-full"
+          src="https://tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+          lazy-src="https://picsum.photos/id/11/10/6"
+          max-height="150"
+          max-width="250"
+        />
+        <div class="p-6 px-6 py-4 pb-2">
+          <div class="font-bold text-xl mb-2 text-no-wrap">
+            The Coldest Sunset
+          </div>
+          <p class="text-gray-700 text-base text-no-wrap">
+            Death is like the wind
+          </p>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row class="v-row">
+      <v-col class="max-w-sm rounded overflow-hidden shadow-lg mb-2">
+        <v-img
+          class="w-full"
+          src="https://tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+          lazy-src="https://picsum.photos/id/11/10/6"
+          max-height="150"
+          max-width="250"
+        />
+        <div class="px-6 py-4 pb-2">
+          <div class="font-bold text-xl mb-2 text-no-wrap">
+            The Coldest Sunset
+          </div>
+          <p class="text-gray-700 text-base text-no-wrap">
+            Death is like the wind
+          </p>
+        </div>
+      </v-col>
+      <v-col class="max-w-sm rounded overflow-hidden shadow-lg mb-2">
+        <v-img
+          class="w-full"
+          src="https://tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+          lazy-src="https://picsum.photos/id/11/10/6"
+          max-height="150"
+          max-width="250"
+        />
+        <div class="px-6 py-4 pb-2">
+          <div class="font-bold text-xl mb-2 text-no-wrap">
+            The Coldest Sunset
+          </div>
+          <p class="text-gray-700 text-base text-no-wrap">
+            Death is like the wind
+          </p>
+        </div>
+      </v-col>
+      <v-col class="max-w-sm rounded overflow-hidden shadow-lg mb-2">
+        <v-img
+          class="w-full"
+          src="https://tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+          lazy-src="https://picsum.photos/id/11/10/6"
+          max-height="150"
+          max-width="250"
+        />
+        <div class="px-6 py-4 pb-2">
+          <div class="font-bold text-xl mb-2 text-no-wrap">
+            The Coldest Sunset
+          </div>
+          <p class="text-gray-700 text-base text-no-wrap">
+            Death is like the wind
+          </p>
+        </div>
+      </v-col>
+      <v-col class="max-w-sm rounded overflow-hidden shadow-lg mb-2">
+        <v-img
+          class="w-full"
+          src="https://tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+          lazy-src="https://picsum.photos/id/11/10/6"
+          max-height="150"
+          max-width="250"
+        />
+        <div class="px-6 py-4 pb-2">
+          <div class="font-bold text-xl mb-2 text-no-wrap">
+            The Coldest Sunset
+          </div>
+          <p class="text-gray-700 text-base text-no-wrap">
+            Death is like the wind
+          </p>
+        </div>
+      </v-col>
+      <v-col class="max-w-sm rounded overflow-hidden shadow-lg mb-2">
+        <v-img
+          class="w-full"
+          src="https://tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+          lazy-src="https://picsum.photos/id/11/10/6"
+          max-height="150"
+          max-width="250"
+        />
+        <div class="px-6 py-4 pb-2">
+          <div class="font-bold text-xl mb-2 text-no-wrap">
+            The Coldest Sunset
+          </div>
+          <p class="text-gray-700 text-base text-no-wrap">
+            Death is like the wind
+          </p>
+        </div>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style lang="postcss" scoped>
+.badge {
+  @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
+  &:hover {
+    @apply bg-gray-300;
+  }
+}
+</style>
+<style>
+.v-row {
+  padding-left: 40px;
+  padding-top: 20px;
+}
+* {
+  box-sizing: border-box;
+}
+</style>
